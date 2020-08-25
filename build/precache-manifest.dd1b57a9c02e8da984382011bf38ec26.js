8715e04ab26d7e98ab5bafee5572523cc3d31f88
Merrin Macleod <merrin.macleod@gmail.com>
2020-08-26 11:45:19 +1200

    update name and build

---
+++ build/precache-manifest.dd1b57a9c02e8da984382011bf38ec26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f587381c13c7e856d5916310507493",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5abc9a9123360ccbdd6",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "0c5f8048ef932bb2990c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f5abc9a9123360ccbdd6",
+    "url": "/static/js/2.334bf44e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.334bf44e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c5f8048ef932bb2990c",
+    "url": "/static/js/main.54a93f8c.chunk.js"
+  },
+  {
+    "revision": "89f15dbfd6975aab4678",
+    "url": "/static/js/runtime-main.12c9e4b6.js"
+  }
+]);
