0a778d95de46801036447ad3d85d9ea0a6bc695f
Merrin Macleod <merrin.macleod@gmail.com>
2020-08-26 11:50:49 +1200

    relative paths

---
+++ docs/build/precache-manifest.2d24390dc83fa95c1199300de5bcf1ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "271cacf90bbccc2a23183e0453668e7a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f5abc9a9123360ccbdd6",
+    "url": "./static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "6fe50b51e2d909b3cad3",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f5abc9a9123360ccbdd6",
+    "url": "./static/js/2.334bf44e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.334bf44e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fe50b51e2d909b3cad3",
+    "url": "./static/js/main.7ea83d15.chunk.js"
+  },
+  {
+    "revision": "ddece21001d9ad79331c",
+    "url": "./static/js/runtime-main.03970cc3.js"
+  }
+]);
