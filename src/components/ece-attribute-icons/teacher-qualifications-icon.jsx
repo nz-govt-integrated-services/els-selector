a31f4d3568a442835a5a50091e72bfbb5854ada5
Merrin Macleod <merrin.macleod@gmail.com>
2020-09-02 11:57:11 +1200

    teacher qualifications

--- src/components/ece-attribute-icons/teacher-qualifications-icon.jsx
+++ src/components/ece-attribute-icons/teacher-qualifications-icon.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './teacher-qualifications-icon.scss';
+
 export default function TeacherQualificationsIcon(props) {
   return (
-    <div>{props.data.always && props.data.always.minimum_percent_qualified}</div>
+    <div className="teacher-qualifications-icon__container">
+      {
+        [100,90,80,70,60,50,40,30,20,10,0].map(qualificationLevel => (
+          <span
+            className={`teacher-qualifications-icon teacher-qualifications-icon--${(qualificationLevel <= props.data.always.minimum_percent_qualified) ? 'always' : (props.data.sometimes && qualificationLevel <= props.data.sometimes.minimum_percent_qualified ? 'sometimes' : 'never')}`}>
+          </span>
+        ))
+      }
+    </div>
   )
 }
 
