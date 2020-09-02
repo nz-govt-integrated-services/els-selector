5706425efcd4582d49c3cffba807187a5941dda3
Merrin Macleod <merrin.macleod@gmail.com>
2020-09-02 12:14:19 +1200

    parent involvement

--- src/components/ece-attribute-icons/parent-involvement-icon.jsx
+++ src/components/ece-attribute-icons/parent-involvement-icon.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './parent-involvement-icon.scss';
+
 export default function ParentInvolvementIcon(props) {
+  const inRange = (number, minimum) => {
+    return(minimum && minimum >= number)
+  }
+
   return (
-    <div>{props.data.always && props.data.always.minimum_percent}</div>
+    <div class="involvement-icon__container">
+      {
+        [100,80,60,40,20,0].map(involvementLevel => (
+          <span
+            class={`involvement-icon involvement-icon--${inRange(involvementLevel, props.data.always.minimum_percent) ? 'always' : (inRange(involvementLevel, props.data.sometimes.minimum_percent) ? 'sometimes' : 'never')}`}
+          >
+          </span>
+        ))
+      }
+    </div>
   )
 }
 
