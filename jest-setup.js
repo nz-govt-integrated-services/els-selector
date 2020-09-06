c9633542ff539b3c0daaa303fa865d8a4ed9e113
Merrin Macleod <merrin.macleod@gmail.com>
2020-09-07 08:51:26 +1200

    Updates

--- jest-setup.js
+++
@@ -1,7 +0,0 @@
-// enzyme configuration
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() });
-
-jest.useFakeTimers();
--- src/components/utils/idSafeString.js
+++
@@ -1,3 +0,0 @@
-export default function idSafeString(string) {
-  return string.replace(/[ ()']/g, '');
-}
