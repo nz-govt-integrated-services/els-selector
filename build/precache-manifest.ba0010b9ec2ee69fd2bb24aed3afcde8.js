6686dc813619f7a25dee738b91eb4d74567ed9b2
arkayed <70299233+arkayed@users.noreply.github.com>
2020-09-07 21:14:57 +1200

    testing

---
+++ build/precache-manifest.ba0010b9ec2ee69fd2bb24aed3afcde8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a9d133cb7f3b9a76bc2843b1f1914b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4a89dc827a73f938e8f6",
+    "url": "./static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "e2182e9f8eaf1161d77b",
+    "url": "./static/css/main.f4d75bb9.chunk.css"
+  },
+  {
+    "revision": "4a89dc827a73f938e8f6",
+    "url": "./static/js/2.97dc383c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.97dc383c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2182e9f8eaf1161d77b",
+    "url": "./static/js/main.35514059.chunk.js"
+  },
+  {
+    "revision": "ddece21001d9ad79331c",
+    "url": "./static/js/runtime-main.03970cc3.js"
+  }
+]);
