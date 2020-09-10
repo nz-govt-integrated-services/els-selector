06f0f6559a8fb7e803e664085e1ca47c34794b87
Merrin Macleod <merrin.macleod@gmail.com>
2020-09-10 15:25:36 +1200

    make it work

--- src/components/tupac/tupac.jsx
+++ src/components/tupac/tupac.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
 
 import EceType from '../ece-type/ece-type';
 import ComparisonTable from '../comparison-table/comparison-table';
@@ -8,6 +7,17 @@ import TABLE_DATA from '../../data/table-data.json';
 
 import KINDY_DATA from '../../data/ece-types/kindergarten.json';
 import KOHANGA_DATA from '../../data/ece-types/kohanga.json';
+import AKOTEU_DATA from '../../data/ece-types/akoteu.json';
+import AOGA_DATA from '../../data/ece-types/aoga.json';
+import HOME_BASED_DATA from '../../data/ece-types/home-based.json';
+import EDUCATION_AND_CARE_DATA from '../../data/ece-types/education-and-care.json';
+import MONTESSORI_DATA from '../../data/ece-types/montessori.json';
+import PLAYCENTRE_DATA from '../../data/ece-types/playcentre.json';
+import PLAYGROUP_DATA from '../../data/ece-types/playgroup.json';
+import PUNA_KOHUNGAHUNGA_DATA from '../../data/ece-types/puna-kohungahunga.json';
+import PUNA_REO_DATA from '../../data/ece-types/puna-reo.json';
+import STEINER_DATA from '../../data/ece-types/steiner.json';
+import TE_KURA_DATA from '../../data/ece-types/te-kura.json';
 
 export default function Tupac() {
   const [selectedType, setSelectedType] = useState(null);
@@ -15,6 +25,22 @@ export default function Tupac() {
   const handleClick = (type) => {
     setSelectedType(type);
   };
+
+  const eceTypeData = {
+    "kindergarten": KINDY_DATA,
+    "kohanga": KOHANGA_DATA,
+    "akoteu": AKOTEU_DATA,
+    "aoga": AOGA_DATA,
+    "education-and-care": EDUCATION_AND_CARE_DATA,
+    "montessori": MONTESSORI_DATA,
+    "playcentre": PLAYCENTRE_DATA,
+    "playgroup": PLAYGROUP_DATA,
+    "puna_kōhungahunga": PUNA_KOHUNGAHUNGA_DATA,
+    "puna_reo": PUNA_REO_DATA,
+    "steiner": STEINER_DATA,
+    "te_kura": TE_KURA_DATA,
+    "home_based": HOME_BASED_DATA
+  }
 
   return (
     <div className="container-fluid">
@@ -25,8 +51,7 @@ export default function Tupac() {
         {
           selectedType && (
             <div className="col-4">
-              <h2>{selectedType}</h2>
-              <EceType data={ KINDY_DATA } handleClick={ handleClick } />
+              <EceType data={ eceTypeData[selectedType] } handleClick={ handleClick } />
             </div>
           )
         }
