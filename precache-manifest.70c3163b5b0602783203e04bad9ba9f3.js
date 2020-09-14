58c9691aebfd0f969c415c4099010627d8306b2b
mermop <mermop@users.noreply.github.com>
2020-09-14 01:59:52 +0000

    deploy: 674d90004b3f79642484367cfc3c26939818a8d8

---
+++ precache-manifest.70c3163b5b0602783203e04bad9ba9f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46578214c6fb5a1a17730e4fd0fe5c13",
+    "url": "./index.html"
+  },
+  {
+    "revision": "51f41f28913ea30ef880",
+    "url": "./static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "9cf5e74ab910e45a6c4e",
+    "url": "./static/css/main.7d22cc0b.chunk.css"
+  },
+  {
+    "revision": "51f41f28913ea30ef880",
+    "url": "./static/js/2.34817a5f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.34817a5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cf5e74ab910e45a6c4e",
+    "url": "./static/js/main.9b5db6a8.chunk.js"
+  },
+  {
+    "revision": "ddece21001d9ad79331c",
+    "url": "./static/js/runtime-main.03970cc3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.33ea476a1cbafa64191a798619b74628.js"
+  "./precache-manifest.70c3163b5b0602783203e04bad9ba9f3.js"
 );
 
 self.addEventListener('message', (event) => {
