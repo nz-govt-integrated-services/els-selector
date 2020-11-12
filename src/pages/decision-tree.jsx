6bbf913dd0fa3f88592fe5b17fc5649e26632581
arkayed <70299233+arkayed@users.noreply.github.com>
2020-11-13 08:24:43 +1300

    Some DT editing

--- src/pages/decision-tree.jsx
+++ src/pages/decision-tree.jsx
@@ -13,8 +13,7 @@ function DecisionTreePage() {
           <div className="col-12">
             <h1 className="mb-4">What type of early learning service is right for you?</h1>
             <p>Unsure which type of ELS will best suit your needs and the needs of your child and whānau? Simply select your preferred scenarios below and see what type of ELS we suggest might work for you.</p>
-            <p>  </p>
-            <p>All early learning services operate differently. Please arrange a visit with your shortlisted centres to make sure that they deliver on the criteria that is important to you. </p>
+            <p>  </p>            
           </div>
         </div>
         <DecisionTree
